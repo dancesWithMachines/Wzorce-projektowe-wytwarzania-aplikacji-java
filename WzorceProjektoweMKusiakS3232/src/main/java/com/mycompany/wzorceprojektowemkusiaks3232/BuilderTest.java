758bb3b81416464f08e76cd2e535dd380b0ffdd0
Mateusz Kusiak (Timax) <timax2@wp.pl>
2021-05-04 18:05:04 +0200

    Updates

---
+++ WzorceProjektoweMKusiakS3232/src/main/java/com/mycompany/wzorceprojektowemkusiaks3232/BuilderTest.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.wzorceprojektowemkusiaks3232;
+
+import java.util.List;
+
+/**
+ *
+ * @author Timax
+ */
+public class BuilderTest {
+    public static void main(String[] args){
+        VetSalonOwner manager = new VetSalonOwner();
+        
+        DogVet dogVet = new DogVet();
+        CatVet catVet = new CatVet();
+        
+        manager.makeVetHelp(dogVet);
+        
+        List<String> dogMedicamentsList = dogVet.getListOfMedicationsAdministered().getMedicamentsList();
+        System.out.println("Dog medicaments:");
+        for (String medicament: dogMedicamentsList)
+            System.out.println(medicament);
+       
+        manager.makeVetHelp(catVet);
+        
+        List<String> catMedicamentsList = catVet.getListOfMedicationsAdministered().getMedicamentsList();
+        System.out.println("Cat medicaments:");
+        for (String medicament: catMedicamentsList)
+            System.out.println(medicament);
+        
+        
+    }
+}
