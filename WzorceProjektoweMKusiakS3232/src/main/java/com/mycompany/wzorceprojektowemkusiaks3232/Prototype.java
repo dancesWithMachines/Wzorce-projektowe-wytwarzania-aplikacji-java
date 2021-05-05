2b6e5c9a2e1992b5189b98961bec739b1a76e756
Mateusz Kusiak (Timax) <timax2@wp.pl>
2021-05-05 12:10:07 +0200

    Updates

---
+++ WzorceProjektoweMKusiakS3232/src/main/java/com/mycompany/wzorceprojektowemkusiaks3232/Prototype.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.wzorceprojektowemkusiaks3232;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Timax
+ */
+abstract class Prototype {
+    public Prototype(String name){
+        this.name = name;
+    }
+    
+    public String name;
+    
+    public abstract Prototype clonePrototype();
+}
+
+class PrototypeA extends Prototype{
+
+    public PrototypeA(String name) {
+        super(name);
+    }
+
+    @Override
+    public Prototype clonePrototype() {
+        return new PrototypeA(this.name);
+    }
+    
+}
+
+class PrototypeB extends Prototype{
+
+    public PrototypeB(String name) {
+        super(name);
+    }
+
+    @Override
+    public Prototype clonePrototype() {
+        return new PrototypeB(this.name);
+    }
+    
+}
---
+++ WzorceProjektoweMKusiakS3232/src/main/java/com/mycompany/wzorceprojektowemkusiaks3232/PrototypeTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.wzorceprojektowemkusiaks3232;
+
+/**
+ *
+ * @author Timax
+ */
+public class PrototypeTest {
+    public static void main(String[] args){
+        PrototypeA prototypeA = new PrototypeA("First");
+        PrototypeA prototypaAClone = (PrototypeA) prototypeA.clonePrototype();
+        System.out.println("Cloned prototype name:" + prototypaAClone.name);
+        
+        PrototypeB prototypeB = new PrototypeB("Second");
+        PrototypeB prototypaBClone = (PrototypeB) prototypeB.clonePrototype();
+        System.out.println("Cloned prototype name:" + prototypaBClone.name);
+    }
+}
+    
